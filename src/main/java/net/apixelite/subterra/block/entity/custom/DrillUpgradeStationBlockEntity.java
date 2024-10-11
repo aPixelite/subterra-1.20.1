2063c5c15ca2dff826e41151779dfb2b12adbe64
aPixelite <115071005+aPixelite@users.noreply.github.com>
2024-10-11 13:42:54 +0200

    start of enchantments

--- src/main/java/net/apixelite/subterra/block/entity/custom/DrillUpgradeStationBlockEntity.java
+++ src/main/java/net/apixelite/subterra/block/entity/custom/DrillUpgradeStationBlockEntity.java
@@ -14,7 +14,6 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.RegistryWrapper.WrapperLookup;
